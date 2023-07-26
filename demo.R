38f3e1d7c3ee5c6ed6eb5af32d4ce41526ab8337
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-07-26 15:05:21 +0200

    add a demo R script

---
+++ demo.R
@@ -0,0 +1,27 @@
+library(readr)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
+# It is good practice to define all calls to `library()` at the top of the script
+# You may not need all of these dependencies, so trim them as you see fit.
+
+
+# import ------------------------------------------------------------------
+## add code here
+
+
+# tidy --------------------------------------------------------------------
+## add code here
+
+
+# transform ---------------------------------------------------------------
+## add code here
+
+
+# visualize ---------------------------------------------------------------
+## add code here
+
+
+# model -------------------------------------------------------------------
+## add code here
